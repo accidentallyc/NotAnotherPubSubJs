002528d5571fecbc220792ed765a54f6bbef8694
accidentallyc <project.cassey@gmail.com>
2016-03-30 00:53:12 +0800

    Refacted to classes - removed some features

--- js/notanotherpubsub.js
+++ js/notanotherpubsub.js
@@ -2,124 +2,82 @@
 var topics      = {}    // Dictionary of all topics
 var publist     = {}    // Dictionary of all published topics to allow late subscriptions to get published
 var func_index  = {}    // Index of optional_id's, used for removing functions
+var ID_SERIAL   = 1     // incemented for every subscriber
 window.subscribe = subscribe
 window.publish = publish
-window.unsubscribe = unsubscribe
 
-/**
-* @return Id of the subscription 
-*/
-function subscribe(topic,func,optional_id){     // Basic subscribe interface
-    
-    var sub_index
-    if( topic instanceof Array ) {
-        var sorted_topics = topic.sort()
-        sub_info = pubsub_multi_subscribe(sorted_topics,func);
+function Topic(){
+  this.subscribers = []
+  this.subscribe = function( subscriber ){
+    this.subscribers.push( subscriber )
+  }
+
+  this.publish = function( data ){
+    for( var i=0; i< this.subscribers.length; i++){
+      var sub = this.subscribers[i]
+      sub.execute(data)
     }
-    else{
-        sub_info = pubsub_single_subscribe(topic,func);
-    }
-
-    func_index[ optional_id || sub_info.id ] = sub_info
-    return sub_info.id
-}
-        
-
-function pubsub_single_subscribe(topic,func){
-    if( topic in  publist ){
-        var item = publist[topic]
-        func.apply( item.valueForThis,item.args);
-    } 
-    
-    var temp         = topics[topic];
-    if( !temp ) temp = topics[topic] = [];
-    var new_index    = topics[topic].length
-    temp.push( func );
-
-    //return the index of the newly inserted function
-    return {
-        index: new_index,
-        topic: topic,
-        id: topic + topics[topic].length
-    }    
-}//func pubsub_single_subscribe
-    
-
-function pubsub_multi_subscribe(topics,func){
-    return (function(){
-        var call_func = func
-        var call_topic = "__callback__:" + topics.join("+")
-        var call_counter = 0
-        var call_args = {}
-        window.cargs = call_args
-        /**
-         * Subscribe original function to generated key
-         */
-        //log.trace("Pubsub Merge Call Topic Generated: " + call_topic);
-        var sub_info = pubsub_single_subscribe( call_topic, func );
-    
-        /*
-         * Subscribe to all the topics just to
-         * increment the counter
-         */
-        for(var i = 0; i < topics.length; i++){
-            !function(){
-                var topic   = topics[i];
-                pubsub_single_subscribe(topic, function(){
-                    //log.trace("Pubsub Merge Counter ++ for topic: " + call_topic);
-                    call_args[ topic ] = arguments;
-                    call_counter++;
-                    if( call_counter == topics.length ){
-                       publish(call_topic, call_args);
-                    }
-                });
-            }();//anonymous scoping function;
-        }
-        return sub_info
-    }());
-}//func pubsub_multi_subscribe
-    
-function publish(topic,args,valueForThis){
-    var topic = topic.trim();
-    var temp = topics[topic];
-    publist [ topic ] = {args:args,valueForThis:valueForThis}
-
-    if( temp  && temp.length  ){
-        //log.debug("Publishing topic `" + topic + "` containing ", temp.length + " functions ");
-        if( args ){
-            if( !(args instanceof Array) ){
-                args = [args];
-            }
-        }else{
-            args = [];
-        }
-        if( !valueForThis ) valueForThis = null;
-        var return_result = true;
-        for( var x=0;x<temp.length;x++){
-            if( !temp[x] ) continue
-            return_result = temp[x].apply(valueForThis,args);
-            if( return_result === false ){
-                return false;
-            }
-        }
-        return return_result;               
-    }else{
-        //log.debug("Publishing topic `" + topic + "` containing :", "0 functions ");
-    }
-};
-
-function unsubscribe(id){
-    var target = func_index[ id ]
-    var old_func = topics[ target.topic ][target.index]
-    topics[ target.topic ][target.index] = null
-
-    return old_func
+  }
 }
 
-    
-    
-}()     
+Topic.topics = {}
+Topic.get = function( topic ){
+  var temp = Topic.topics[ topic ]
+  if( !temp ){
+    // If not exists then create it
+    // then save it
+    temp = new Topic(topic)
+    Topic.topics[ topic ] = temp
+  }
+  return temp
+}
 
-    
-    
-    
+function Subscriber(){
+  // Funcs
+  this.subscribe  = subscribe
+  this.execute    = execute
+
+  // Fields
+  this.id         = ID_SERIAL++ // Generated on subscription
+  this.isStarted  = true
+
+  function execute(data){
+    if( this.isStarted )
+      this.callback.apply(null,data)
+  }
+
+  function subscribe(topic,callback){
+    this.topics   = topic instanceof Array  ? topic.sort() : new Array(topic)
+    this.callback = callback
+
+    var subscriber = this
+    this.topics.forEach(function( topic, index ){
+      var instance = Topic.get( topic )
+      instance.subscribe( subscriber )
+    })
+  }
+
+  function stop(){
+    this.isStarted = false
+  }
+
+  function start(){
+    this.isStarted = true
+  }
+}
+
+
+function subscribe(topic,callback){     // Basic subscribe interface
+  var sub = new Subscriber()
+  sub.subscribe( topic,callback )
+
+  return sub
+}
+
+function publish(topic,data){
+  var instance = Topic.get( topic )
+  instance.publish(data)
+}
+
+
+}()
